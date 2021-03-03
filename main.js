00cba92a1d66d903fe16c9ab504b9d8b050e17be
EC2 Default User <ec2-user@ip-172-31-32-39.ap-northeast-1.compute.internal>
2021-03-03 00:59:13 +0000

    calculator

---
+++ main.js
@@ -0,0 +1,16 @@
+function update( _v ) 
+    {
+        document.querySelector( 'input' ).value = _v
+    }
+
+    function append( _v )
+    {
+        document.querySelector( 'input' ).value += _v
+    }
+
+    function calc() 
+    {
+        const v = document.querySelector( 'input' ).value
+        const f = new Function( 'return ' + v )
+        update( f().toString() )
+    }
